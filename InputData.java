dd2b3b215677e3daf87c51e9d43591975bec7ac1
hayabu5a <pro.hayabusa5@gmail.com>
2019-02-04 20:49:54 +0530

    Create InputData.java

---
+++ InputData.java
@@ -0,0 +1,33 @@
+package mainpackage;
+
+import  java.util.Scanner;
+
+public class input {
+
+  public Scanner new=reader Scanner(System.in);
+  public static void main(String[] args){
+  
+    System.out.println("----- INPUT -----");
+    
+    System.out.print("Enter Name: ");
+    String name=reader.nextLine();
+    System.out.print("Enter Age: ");
+    int age=reader.nextInt();
+    System.out.print("Enter Salary: ");
+    double salary=reader.nextDouble();
+    System.out.print("Enter Pi Value: ");
+    float pi=reader.nextFloat();
+    System.out.print("Enter Gender: ");
+    String input=reader.nextLine();
+    char gender=input.charAt(0);
+    
+    System.out.println("----- OUTPUT -----");
+    
+    System.out.println("Name: "+name);
+    System.out.println("Age: "+age);
+    System.out.println("Salary: "+salary);
+    System.out.println("Pi Value: "+pi);
+    System.out.println("Gender: "+gender);
+  
+  }
+}
