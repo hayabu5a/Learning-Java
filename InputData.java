1c00104041a83f4b4de7b526cbc3b70164f31914
hayabu5a <pro.hayabusa5@gmail.com>
2019-02-05 12:59:15 +0530

    Update InputData.java

--- InputData.java
+++ InputData.java
@@ -26,10 +26,19 @@ public class input {
     System.out.println("----- OUTPUT -----");
     
     System.out.println("Name: "+name);
-    System.out.println("Age: "+age);
-    System.out.println("Salary: "+salary);
+    System.out.println("Age: "+age+" Years");
+    System.out.println("Salary: $"+salary);
     System.out.println("Pi Value: "+pi);
     System.out.println("Gender: "+gender);
   
   }
 }
+
+/*
+- OUTPUT
+Name: Smith
+Age: 20 Years
+Salary: $3500.50
+Pi Value: 3.14
+Gender: M
+*/
